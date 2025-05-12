edf6ff75d6ee73b885a64d1d1452bf978c350c58
Luiz Caetano <luizindsc@proton.me>
2025-05-12 10:25:48 -0300

    create costumer registor

--- clientes.php
+++ clientes.php
@@ -8,13 +8,45 @@ require('conexao.php');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Clientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   </head>
   <body>
 
+    <?php include('navbar.php'); ?>
     
+     <div class="container mt-4">
+      <?php include('mensagem.php'); ?>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4> Clientes
+                <a href="criar-cliente.php" class="btn btn-primary float-end">Cadastrar Cliente</a>
+              </h4>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>CNPJ-CPF</th>
+                    <th>Endereço</th>
+                    <th>Telefone</th>
+                    <th>Cidade</th>
+                  </tr>
+                </thead>
+                <tbody>
 
+
+
+
+
+
+
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
   </body>
---
+++ criar-cliente.php
@@ -0,0 +1,16 @@
+<?php include ('heade.php'); ?>
+
+<body>
+    <?php include ('navbar.php'); ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
